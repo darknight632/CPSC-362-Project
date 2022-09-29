6c415954922d6f54e51dbf65c9a9eb2a6b113784
atran632 <46984486+darknight632@users.noreply.github.com>
2022-09-29 16:18:34 -0700

    git pages

--- Test/src/contexts/ContextProvider.js
+++ Test/src/contexts/ContextProvider.js
@@ -5,7 +5,7 @@ const StateContext = createContext();
 const initialState = { chat: false, userProfile: false, Notification: false }
 
 export const ContextProvider = ({ children }) => {
-    const [activeMenu, setActiveMenu] = useState(true);
+    const [activeMenu, setActiveMenu] = useState(false);
     const [isClicked, setisClicked] = useState(initialState);
     const handleClicked = (clicked) => setisClicked({...initialState,[clicked] : true});
     return (<StateContext.Provider value={{ activeMenu, setActiveMenu,isClicked,setisClicked,handleClicked,initialState }}>
