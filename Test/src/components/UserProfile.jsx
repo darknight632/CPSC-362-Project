646523720e70568af7afbf091352416bfe7216ce
atran632 <46984486+darknight632@users.noreply.github.com>
2022-10-06 14:00:31 -0700

    fixed user dropdown

--- Test/src/components/UserProfile.jsx
+++ Test/src/components/UserProfile.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { MdOutlineCancel } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
 import { Button } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 import { userProfileData } from "../data/dummy";
@@ -12,7 +11,7 @@ const UserProfile = () => {
     <div className="nav-item absolute right-1 top-16 bg-white p-8 rounded-lg w-96">
       <div className="flex justify-between items-center">
         <p className="font-semibold text-lg dark:text-gray-200">User Profile</p>
-        <button
+        <Button
           icon={<MdOutlineCancel />}
           color="rgb(153,171,180)"
           bgHoverColor="light-gray"
