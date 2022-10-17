0d74b9aa4a8990000c5c5dc07caf371f05a1aa37
atran632 <46984486+darknight632@users.noreply.github.com>
2022-10-16 18:59:38 -0700

    login stil needs to be fixed

--- Test/src/contexts/ContextProvider.js
+++ Test/src/contexts/ContextProvider.js
@@ -8,6 +8,7 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(false);
   const [isClicked, setisClicked] = useState(initialState);
   const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentLogin, setLogin] = useState(false);
 
   const handleClicked = (clicked) => setisClicked({ ...initialState, [clicked]: true });
 
@@ -17,6 +18,8 @@ export const ContextProvider = ({ children }) => {
       value={{
         currentColor,
         setCurrentColor,
+        currentLogin,
+        setLogin,
         activeMenu,
         setActiveMenu,
         isClicked,
