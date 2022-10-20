c44729a841401c072d5b98e325ac35ef9458f686
Mario Linares <mariolinares@csu.fullerton.edu>
2022-10-20 11:28:42 -0700

    Revert "Profile page"
    This reverts commit f48ebac01da3c0eab2d12728d899a5362d84ceb2.

--- Test/src/pages/Exercise.jsx
+++ Test/src/pages/Exercise.jsx
@@ -1,48 +1,22 @@
-import { Button } from '@mui/material';
 import React, {useEffect,useState} from 'react';
-import './Profile.css'
+import { Box } from '@mui/material';
+
+import Search from '../components/Exercise/Search';
+import ExerciseMain from '../components/Exercise/ExerciseMain';
+
+
 
 const Exercise = () => {
 
+const [exercises, setExercises] = useState([]);
+const [bodyPart,setBodyPart] = useState('all');
+
   return (
-    <body>
+    <Box>
+      <Search setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <ExerciseMain setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
 
-      <div className='card'>
-
-        <div className='profile-pic'>
-          <img src="../pages/generic_user.png" />
-        </div>
-
-      <div classname="profile-name">
-          <h2>Bob joe</h2>
-          <p>@BobJoe</p>
-
-      </div>
-
-      <div class="profile-button">
-        <Button>Weight</Button>
-        <Button>Height</Button>
-      </div>
-
-      <div className='detail'>
-
-          <div className='follower'>
-            <h4>Gender</h4>
-            <p>Age</p>
-          </div>
-
-          <div className='height'>
-            <h4>Male</h4>
-            <p>23</p>
-          </div>
-      </div>
-
-      <div className='know-more'></div>
-
-      </div>
-      
-    </body>
-
+    </Box>
   )
 }
 
