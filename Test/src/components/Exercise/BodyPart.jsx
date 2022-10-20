760b7dfe2f445ee7aa2b947230747e34bf0f5409
atran632 <46984486+darknight632@users.noreply.github.com>
2022-10-19 23:03:58 -0700

    minor changes to wrkout api

--- Test/src/components/Exercise/BodyPart.jsx
+++ Test/src/components/Exercise/BodyPart.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
-import Icon from '../../assets/icons/gym.png';
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
   <Stack
@@ -14,7 +13,7 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
   >
-    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+    <button alt="dumbbell" style={{ width: '40px', height: '40px' }} />
     <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
   </Stack>
 );
