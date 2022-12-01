7f32e65b07f20be0b86a4b1be6a29775b91d5e73
atran632 <46984486+darknight632@users.noreply.github.com>
2022-11-30 23:32:05 -0800

    test

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,105 +2,150 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    const [details, setDetails] = useState({ name: "", email: "", password: "", weight: "",Age: "" });
+    const [firstname, setFirstName] = useState("");
+    const [lastname,setLastName] = useState("");
+    const [S_ans,setS_ans] = useState("");
+    const [gender,setGender] = useState("");
+    const [password, setPassword] = useState("");
+    const [weight, setWeight] = useState("");
+    const [age, setAge] = useState("");
+    const [height,setHeight] = useState("");
+    const [username,setUsername] = useState("");
+    const [message, setMessage] = useState("");
+    const [calorie,setCalorie] = useState("");
     const navigate = useNavigate();
 
-    const submitHandler = (e) => {
+    let submitHandler = async (e) => {
         e.preventDefault();
-    };
+        try{
+            let res = await fetch("http://127.0.0.1:8000/client?", {
+                method: "POST",
+                body: JSON.stringify({
+                    "user_names":username,
+                    "first_name":firstname,
+                    "last_name":lastname,
+                    "passwords":password,
+                    "security_question":"color",
+                    "security_answer":S_ans,
+                    "gender":gender,
+                    "age":age,
+                    "height":height,
+                    "weights":weight,
+                    "calorie_intake_per_day":calorie,
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            });
 
-    const adminUser = {
-        email: "admin@admin.com",
-        password: "12",
-      };
-    
-      const [user, setUser] = useState({ name: "", email: "" });
-      const [error, setError] = useState("");
-    
-      const Login = (details) => {
-        console.log(details);
-    
-        if (
-          details.email == adminUser.email &&
-          details.password == adminUser.password
-        ) {
-          console.log("Logged in");
-          setUser({
-            name: details.name,
-            email: details.email,
-          });
-        } else {
-          console.log("Username or password incorrect!");
-          setError("Username or password incorrect!");
+            let resJson = await res.json();
+            if(res.status === 200){
+                setName("");
+                setMessage("User create successfully");
+            }else{
+                setMessage("Some error occured");
+            }
+        } catch(err){
+            console.log(err);
         }
-      };
+    };
     
     return (
         <form onSubmit={submitHandler}>
             <div className="App">
                 <div className="form-inner">
                     <h2>Register</h2>
-                    {error != "" ? <div className="error">{error}</div> : ""}
                     <div className="form-group">
-                        <label htmlFor="name">Name:</label>
+                        <label htmlFor="name">First Name:</label>
                         <input
                             type="text"
-                            name="name"
-                            id="name"
-                            onChange={(e) => setDetails({ ...details, name: e.target.value })}
-                            value={details.name}
+                            value={firstname}
+                            placeholder="First Name"
+                            onChange={(e) => setFirstName(e.target.value)}
                         />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">Email: </label>
+                        <label htmlFor="name">Last Name:</label>
                         <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            onChange={(e) =>
-                                setDetails({ ...details, email: e.target.value })
-                            }
-                            value={details.email}
+                            type="text"
+                            value={lastname}
+                            placeholder="Last Name"
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="name">Username</label>
+                        <input
+                            type="text"
+                            value={username}
+                            placeholder="Username"
+                            onChange={(e) => setUsername(e.target.value)}
                         />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password:</label>
                         <input
                             type="password"
-                            name="password"
-                            id="password"
-                            onChange={(e) =>
-                                setDetails({ ...details, password: e.target.value })
-                            }
-                            value={details.password}
+                            value={password}
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <div className="form-group">
                         <label htmlFor="weight">Weight: </label>
                         <input
                             type="text"
-                            name="Weight"
-                            id="weight"
-                            onChange={(e) =>
-                                setDetails({ ...details, email: e.target.value })
-                            }
-                            value={details.email}
+                            value={weight}
+                            placeholder="Weight"
+                            onChange={(e) => setWeight(e.target.value)}
                         />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="name">Height:</label>
+                        <input
+                            type="text"
+                            value={height}
+                            placeholder="Height"
+                            onChange={(e) => setHeight(e.target.value)}
+                        />
+                    <div className="form-group">
+                        <label htmlFor="name">Gender:</label>
+                        <input
+                            type="character"
+                            value={gender}
+                            placeholder="Gender"
+                            onChange={(e) => setGender(e.target.value)}
+                        />
+                    </div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="Age">Age: </label>
                         <input
                             type="text"
-                            name="Age"
-                            id="age"
-                            onChange={(e) =>
-                                setDetails({ ...details, email: e.target.value })
-                            }
-                            value={details.email}
+                            value={age}
+                            placeholder="Age"
+                            onChange={(e) => setAge(e.target.value)}
                         />
                     </div>
+                    <div className="form-group">
+                        <label htmlFor="name">Calorie Intake:</label>
+                        <input
+                            type="text"
+                            value={calorie}
+                            placeholder="Calories"
+                            onChange={(e) => setCalorie(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="name">Security Answer:</label>
+                        <input
+                            type="text"
+                            value={S_ans}
+                            placeholder="Answer"
+                            onChange={(e) => setS_ans(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Submit</button>
                     <div>
-                        <button onClick={() => navigate("/")}>Submit</button>
+                        {message ? <p>{message}</p> : null}
                     </div>
                 </div>
             </div>
